d96a0c533c517ef576e1ec7abbbb6a7ab593f379
12345asdfj <140524599+12345asdfj@users.noreply.github.com>
2023-08-02 10:56:45 -0700

    Add files via upload

---
+++ j.js
@@ -0,0 +1,32 @@
+function star() {
+    const invaliu = document.getElementById("size").value;
+    let output = '';
+
+    for (let i = 1; i <= invaliu; i++) {
+      let row = ''
+      for (let j = 1; j <= invaliu; j++) {
+        row += "* ";
+      }
+      output += row + '<br>';
+    }
+
+    document.getElementById("output").innerHTML = output;
+
+  }
+
+
+
+  function stars() {
+    const invaliu = document.getElementById("sizes").value;
+    let output = '';
+
+    for (let i = 1; i <= invaliu; i++) {
+      let row = ''
+      for (let j = 1; j <= i; j++) {
+        row += "* ";
+      }
+      output += row + '<br>';
+    }
+
+    document.getElementById("output").innerHTML = output;
+}
